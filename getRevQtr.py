af7bdfc1294bf05163bcd0377e97807414f3b64a
epistemik <epistemik@gmail.com>
2019-03-26 20:13:47 -0400

    working code -- as an example, gets the Revenue for Mark Salary for any specified quarter

--- getRevQtr.py
+++ getRevQtr.py
@@ -25,7 +25,7 @@
 # @revised Mark Sattolo <epistemik@gmail.com>
 # @version Python 3.6
 # @created 2019-03-24
-# @updated 2019-03-24
+# @updated 2019-03-26
 
 from sys import argv, stdout
 from datetime import date, timedelta, datetime
@@ -44,21 +44,13 @@ REV_ACCTS = {
     "Sal_Lu" : ["REV_Salary", "Lulu", "Lu-Gross"],
 }
 
-# a dictionary with a period name as key, and number of months in that
-# kind of period as the value
+# a dictionary with a period name as key, and number of months in that kind of period as the value
 PERIODS = {
-    "monthly"  :  1,
-    "quarterly":  3,
-    # mhs | add thirdly, halfly, biyearly
-    "thirdly"  :  4,
-    "halfly"   :  6,
-    "yearly"   : 12,
-    "biyearly" : 24
+    "quarterly":  3
 }
 
 NUM_MONTHS = 12
 ONE_DAY = timedelta(days=1)
-DEBITS_SHOW, CREDITS_SHOW = ("debits-show", "credits-show")
 ZERO = Decimal(0)
 
 
@@ -69,7 +61,7 @@ def gnc_numeric_to_python_decimal(numeric):
     copy = GncNumeric(numeric.num(), numeric.denom())
     result = copy.to_decimal(None)
     if not result:
-        raise Exception("gnc numeric value {} CAN'T be converted to decimal!".format(copy.to_string()))
+        raise Exception("gnc numeric value '{}' CAN'T be converted to decimal!".format(copy.to_string()))
 
     digit_tuple = tuple(int(char) for char in str(copy.num()) if char != '-')
     denominator = copy.denom()
@@ -84,14 +76,6 @@ def next_period_start(start_year, start_month, period_type):
 
     # use integer division to find out if the new end month is in a different year, what year it is,
     # and what the end month number should be changed to.
-    # Because this depends on modular arithmetic, we have to convert the month
-    # values from 1-12 to 0-11 by subtracting 1 and putting it back after
-    #
-    # the really cool part is that this whole thing is implemented without
-    # any branching; if end_month > NUM_MONTHS
-    #
-    # Another super nice thing is that you can add all kinds of period lengths to PERIODS
-    # fix Python 2 to Python 3 -- need '//' to do integer division
     end_year = start_year + ((end_month - 1) // NUM_MONTHS)
     end_month = ((end_month - 1) % NUM_MONTHS) + 1
 
@@ -100,16 +84,12 @@ def next_period_start(start_year, start_month, period_type):
 
 def period_end(start_year, start_month, period_type):
     if period_type not in PERIODS:
-        raise Exception("%s is not a valid period, should be %s" % (period_type, str(PERIODS.keys())))
+        raise Exception("'{}' is NOT a valid period >> should be {}!".format(period_type, str(PERIODS.keys())))
 
     end_year, end_month = next_period_start(start_year, start_month, period_type)
 
     # last step, the end date is day back from the start of the next period
-    # so we get a period end like
-    # 2010-03-31 for period starting 2010-01 instead of 2010-04-01
-    # fix Python 2 to Python 3 -- need to fix next_period_start() so end_year is an int rather than a float
-    # print("end_year({}) = {}; end_month({}) = {}; ONE_DAY({}) = {}"
-    #       .format(type(end_year), end_year, type(end_month), end_month, type(ONE_DAY), ONE_DAY))
+    # so we get a period end like 2010-03-31 for period starting 2010-01 instead of 2010-04-01
     return date(end_year, end_month, 1) - ONE_DAY
 
 
@@ -120,19 +100,16 @@ def generate_period_boundaries(start_year, start_month, period_type, periods):
 
 
 def account_from_path(top_account, account_path, original_path=None):
-    # mhs | debug
     # print("top_account = %s, account_path = %s, original_path = %s" % (top_account, account_path, original_path))
     if original_path is None:
         original_path = account_path
     account, account_path = account_path[0], account_path[1:]
-    # mhs | debug
     # print("account = %s, account_path = %s" % (account, account_path))
 
     account = top_account.lookup_by_name(account)
-    # mhs | debug
     # print("account = " + str(account))
     if account is None:
-        raise Exception("path " + str(original_path) + " could not be found")
+        raise Exception("path '" + str(original_path) + "' could NOT be found!")
     if len(account_path) > 0:
         return account_from_path(account, account_path, original_path)
     else:
@@ -143,27 +120,20 @@ def get_splits(acct, period_starts, period_list):
     # insert and add all splits in the periods of interest
     for split in acct.GetSplitList():
         trans = split.parent
-        # fix Python 2 to Python 3 -- Gnucash Py3 bindings: GetDate() returns a datetime, not a timestamp
-        tx_date = trans.GetDate()
-        # print("tx_date({}) = {}".format(type(tx_date), tx_date))
-        # trans_date = date.fromtimestamp(tx_date)
-        trans_date = tx_date.date()
+        # GetDate() returns a datetime
+        tx_datetm = trans.GetDate()
+        # convert to a date
+        trans_date = tx_datetm.date()
 
         # use binary search to find the period that starts before or on the transaction date
         period_index = bisect_right(period_starts, trans_date) - 1
 
-        # ignore transactions with a date before the matching period start
-        # (after subtracting 1 above start_index would be -1)
-        # and after the last period_end
+        # ignore transactions with a date before the matching period start and after the last period_end
         if period_index >= 0 and trans_date <= period_list[len(period_list) - 1][1]:
 
             # get the period bucket appropriate for the split in question
             period = period_list[period_index]
 
-            # more specifically, we'd expect the transaction date to be on or after the period start and before or on
-            # the period end, assuming the binary search (bisect_right) assumptions from above are right...
-            # in other words, we assert our use of binary search
-            # and the filtered results from the above 'if' provide all the protection we need
             assert( period[1] >= trans_date >= period[0] )
 
             split_amount = gnc_numeric_to_python_decimal(split.GetAmount())
@@ -171,18 +141,14 @@ def get_splits(acct, period_starts, period_list):
             # if the amount is negative this is a credit, else a debit
             debit_credit_offset = 1 if split_amount < ZERO else 0
 
-            # store the debit or credit Split with its transaction, using the above offset to get in the right bucket
-            # if we wanted to be really cool we'd keep the transactions
-            period[2 + debit_credit_offset].append((trans, split))
-
             # add the debit or credit to the sum, using the above offset to get in the right bucket
-            period[4 + debit_credit_offset] += split_amount
+            period[2 + debit_credit_offset] += split_amount
 
             # add the debit or credit to the overall total
-            period[6] += split_amount
+            period[4] += split_amount
 
 
-def aa_sum_main():
+def gr_qtr_main():
     exe = argv[0].split('/')[-1]
     if len(argv) < 4:
         print("NOT ENOUGH parameters!")
@@ -201,69 +167,53 @@ def aa_sum_main():
         start_month = (rev_quarter * 3) - 2
 
         gnucash_session = Session(gnucash_file, is_new=False)
-
         root_account = gnucash_session.book.get_root_account()
 
-        account_of_interest = account_from_path(root_account, account_path)
+        account_of_interest = account_from_path(root_account, REV_ACCTS["Sal_Mk"])
         print("account_of_interest = {}".format(account_of_interest.GetName()))
 
         # a list of all the periods of interest
-        # for each period keep the start date, end date, a list to store debits and credits,
-        # and sums for tracking the sum of all debits and sum of all credits
+        # for each period keep the start date, end date, debits and credits sums and overall total
         period_list = [
             [
                 start_date, end_date,
-                [],    # debits
-                [],    # credits
                 ZERO,  # debits sum
                 ZERO,  # credits sum
                 ZERO   # TOTAL
             ]
             for start_date, end_date in generate_period_boundaries(rev_year, start_month, 'quarterly', 1)
         ]
+        print(period_list)
         # a copy of the above list with just the period start dates
         period_starts = [e[0] for e in period_list]
+        print(period_starts)
 
-        # mhs | get the list of all descendant accounts
         descendants = account_of_interest.get_descendants()
 
-        if len(descendants) == 0:
-            # mhs | account has no descendants so just calculate the splits directly
-            get_splits(account_of_interest, period_starts, period_list)
-        else:
-            # mhs | calculate the sums of debits and credits for EACH sub-account but just keep the overall total
-            print("Descendants of %s:" % account_of_interest.GetName())
+        # get the split amounts for the parent account
+        get_splits(account_of_interest, period_starts, period_list)
+        if len(descendants) > 0:
+            # for EACH sub-account add to the overall total
+            print("Descendants of {}:".format(account_of_interest.GetName()))
             for subAcct in descendants:
-                print("{} balance = {}".format(subAcct.GetName(), subAcct.GetBalance()))
+                print("{} balance = {}".format(subAcct.GetName(), gnc_numeric_to_python_decimal(subAcct.GetBalance())))
                 get_splits(subAcct, period_starts, period_list)
 
         # write out the column headers
         csv_writer = csv.writer(stdout)
-        csv_writer.writerow(())
+        csv_writer.writerow('')
         csv_writer.writerow(('period start', 'period end', 'debits', 'credits', 'TOTAL'))
 
-        def generate_detail_rows(values):
-            return (
-                ('', '', '', '', trans.GetDescription(), gnc_numeric_to_python_decimal(split.GetAmount()))
-                for trans, split in values )
-
         # write out the overall totals for the account of interest
-        for start_date, end_date, debits, creds, debit_sum, credit_sum, total in period_list:
+        for start_date, end_date, debit_sum, credit_sum, total in period_list:
             csv_writer.writerow((start_date, end_date, debit_sum, credit_sum, total))
 
-            # write the details for each credit or debit if requested on the command line
-            if debits_show and len(debits) > 0:
-                csv_writer.writerow(('DEBITS', '', '', '', 'description', 'value'))
-                csv_writer.writerows(generate_detail_rows(debits))
-                csv_writer.writerow(())
-            if credits_show and len(creds) > 0:
-                csv_writer.writerow(('CREDITS', '', '', '', 'description', 'value'))
-                csv_writer.writerows(generate_detail_rows(creds))
-                csv_writer.writerow(())
+        tot_revenue = period_list[0][4] * (-1)
+        print("\n{} Revenue for {}-Q{} = {}".format("Sal_Mk", str_year, str_quarter, tot_revenue))
 
         # no save needed, we're just reading..
         gnucash_session.end()
-    except Exception as ae:
+    except Exception as qe:
         if "gnucash_session" in locals() and gnucash_session is not None:
             gnucash_session.end()
         raise
@@ -272,4 +222,4 @@ def aa_sum_main():
 
 
 if __name__ == "__main__":
-    aa_sum_main()
+    gr_qtr_main()
